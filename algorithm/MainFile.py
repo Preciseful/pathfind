02675020708e17138d19f20daa36639b6a4c9283
Developful <preciseful1@gmail.com>
2022-07-29 21:25:33 +0300

    Added files

---
+++ algorithm/MainFile.py
@@ -0,0 +1,12 @@
+import dijkstraFile
+Array = dijkstraFile.Array;
+Vector = dijkstraFile.Vector;
+
+# ^^^ Dont modify above ^^^
+
+
+
+nodePOS = Array(Vector(-26.524, 9.904, 5.853), Vector(-24.405, 8.846, 7.104), Vector(-21.665, 8, 6.262), Vector(-18.721, 8,
+		5.929));
+
+connections = Array(Array(1), Array(0, 2), Array(1, 3), Array(2));
---
+++ algorithm/dijkstraFile.py
@@ -0,0 +1,205 @@
+
+from collections import defaultdict
+from lib2to3.pytree import NodePattern
+import math
+import operator
+from functools import reduce
+
+import pyperclip
+
+def Array(*content): 
+    return list(content)
+
+class Vector:
+	x = 0
+	y = 0
+	z = 0
+
+	def __init__(self, x, y, z):
+		self.x = float(x)
+		self.y = float(y)
+		self.z = float(z)
+
+    # String represntation
+	def __str__(self):
+		return '<%s, %s, %s>' % (self.x, self.y, self.z)
+
+	# Produce a copy of itself
+	def __copy(self):
+		return Vector(self.x, self.y, self.z)
+
+	# Signing
+	def __neg__(self):
+		return Vector(-self.x, -self.y, -self.z)
+
+	# Scalar Multiplication
+	def __mul__(self, number):
+		return Vector(self.x * number, self.y * number, self.z * number)
+
+	def __rmul__(self, number):
+		return self.__mul__(number)
+
+	# Division
+	def __div__(self, number):
+		return self.__copy() * (number**-1)
+
+	# Arithmetic Operations
+	def __add__(self, operand):
+		return Vector(self.x + operand.x, self.y + operand.y, self.z + operand.z)
+
+
+	def __sub__(self, operand):
+		return self.__copy() + -operand
+
+	# Cross product
+	# cross = a ** b
+	def __pow__(self, operand):
+		return Vector(self.y*operand.z - self.z*operand.y, 
+			                self.z*operand.x - self.x*operand.z, 
+			                self.z*operand.y - self.y*operand.x)
+
+	# Dot Project
+	# dp = a & b
+	def __and__(self, operand):
+		return (self.x * operand.x) + \
+		       (self.y * operand.y) + \
+		       (self.z * operand.z)
+ 
+	# Operations
+
+	def normal(self):
+		return self.__copy() / self.magnitude()
+
+	def magnitude(self):
+		return (self.x**2 + self.y**2 + self.z**2)**(.5)
+
+def distance(self, operand):
+        return round(math.sqrt((self.x - operand.x)**2 + (self.y - operand.y)**2 + (self.z - operand.z)**2), 0)
+
+
+
+
+class Graph():
+    def __init__(self):
+        """
+        self.edges is a dict of all possible next nodes
+        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
+        self.weights has all the weights between two nodes,
+        with the two nodes as a tuple as the key
+        e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
+        """
+        self.edges = defaultdict(list)
+        self.weights = {}
+    
+    def add_edge(self, from_node, to_node, weight):
+        # Note: assumes edges are bi-directional
+        if(from_node in connections[to_node] and to_node in connections[from_node]):
+            self.edges[from_node].append(to_node)
+            self.weights[(from_node, to_node)] = weight
+            self.edges[to_node].append(from_node)
+            self.weights[(to_node, from_node)] = weight
+        else:
+            self.edges[from_node].append(to_node)
+            self.weights[(from_node, to_node)] = weight
+            
+
+
+def dijsktra(graph, initial, end):
+    # shortest paths is a dict of nodes
+    # whose value is a tuple of (previous node, weight)
+    shortest_paths = {initial: (None, 0)}
+    current_node = initial
+    visited = set()
+    
+    while current_node != end:
+        visited.add(current_node)
+        destinations = graph.edges[current_node]
+        weight_to_current_node = shortest_paths[current_node][1]
+
+        for next_node in destinations:
+            weight = graph.weights[(current_node, next_node)] + weight_to_current_node
+            if next_node not in shortest_paths:
+                shortest_paths[next_node] = (current_node, weight)
+            else:
+                current_shortest_weight = shortest_paths[next_node][1]
+                if current_shortest_weight > weight:
+                    shortest_paths[next_node] = (current_node, weight)
+        
+        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
+        if not next_destinations:
+            return "Route Not Possible"
+        # next node is the destination with the lowest weight
+        current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
+    
+    # Work back through destinations in shortest path
+    path = []
+    while current_node is not None:
+        path.append(current_node)
+        next_node = shortest_paths[current_node][0]
+        current_node = next_node
+    # Reverse path
+    path = path[::-1]
+    overwatch_graph[initial][end] = path[1];
+    return path
+
+def run_length_encoding(seq):
+  compressed = []
+  count = 1
+  char = seq[0]
+  for i in range(1,len(seq)):
+    if seq[i] == char:
+      count = count + 1
+    else :
+      compressed.append([char,count])
+      char = seq[i]
+      count = 1
+  compressed.append([char,count])
+  return compressed
+
+import MainFile
+
+graph = Graph()
+
+nodePOS = MainFile.nodePOS
+
+connections = MainFile.connections
+
+v = len(nodePOS)
+overwatch_graph = [[0 for x in range(v)] for y in range(v)]
+
+for i in range(v):
+    for t in connections[i]:
+        graph.add_edge(i, t, distance(nodePOS[i], nodePOS[t]))
+
+for i in range(v):
+        for t in range(v):
+            if(i != t):
+                dijsktra(graph, i, t)
+            else:
+                overwatch_graph[i][t] = 999;
+
+stringgraph = ["" for y in range(v)]
+encoded = []
+
+
+unusedalpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáçèéêëìíîïð!@#$%^&*()-_=+[{]};:<,>.?/~`€ƒ‰Œ•"
+alphabet = unusedalpha[0:v+1]
+
+for i in range(v):
+    tempstring = ""
+    
+    for t in overwatch_graph[i]:
+        if(t == 999):
+            tempstring = tempstring + " "
+        else:
+            tempstring = tempstring + alphabet[t]
+
+    stringgraph[i] = tempstring
+
+
+
+print(stringgraph)
+for i 
+print("\n\n\n\nOver")
+pyperclip.copy("variables\n{\nglobal:\n0: nodePOS\n 1: connections\n 2: _arrayConstructor\n}")
+
