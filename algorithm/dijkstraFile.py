32f4b0f121bf43b54a5edc514c828136728e81ef
Developful <preciseful1@gmail.com>
2022-08-03 03:39:59 +0300

    Somewhat cleaned up

--- algorithm/dijkstraFile.py
+++ algorithm/dijkstraFile.py
@@ -20,58 +20,6 @@ class Vector:
 		self.y = float(y)
 		self.z = float(z)
 
-    # String represntation
-	def __str__(self):
-		return '<%s, %s, %s>' % (self.x, self.y, self.z)
-
-	# Produce a copy of itself
-	def __copy(self):
-		return Vector(self.x, self.y, self.z)
-
-	# Signing
-	def __neg__(self):
-		return Vector(-self.x, -self.y, -self.z)
-
-	# Scalar Multiplication
-	def __mul__(self, number):
-		return Vector(self.x * number, self.y * number, self.z * number)
-
-	def __rmul__(self, number):
-		return self.__mul__(number)
-
-	# Division
-	def __div__(self, number):
-		return self.__copy() * (number**-1)
-
-	# Arithmetic Operations
-	def __add__(self, operand):
-		return Vector(self.x + operand.x, self.y + operand.y, self.z + operand.z)
-
-
-	def __sub__(self, operand):
-		return self.__copy() + -operand
-
-	# Cross product
-	# cross = a ** b
-	def __pow__(self, operand):
-		return Vector(self.y*operand.z - self.z*operand.y, 
-			                self.z*operand.x - self.x*operand.z, 
-			                self.z*operand.y - self.y*operand.x)
-
-	# Dot Project
-	# dp = a & b
-	def __and__(self, operand):
-		return (self.x * operand.x) + \
-		       (self.y * operand.y) + \
-		       (self.z * operand.z)
- 
-	# Operations
-
-	def normal(self):
-		return self.__copy() / self.magnitude()
-
-	def magnitude(self):
-		return (self.x**2 + self.y**2 + self.z**2)**(.5)
 
 def distance(self, operand):
         return round(math.sqrt((self.x - operand.x)**2 + (self.y - operand.y)**2 + (self.z - operand.z)**2), 0)
@@ -81,18 +29,11 @@ def distance(self, operand):
 
 class Graph():
     def __init__(self):
-        """
-        self.edges is a dict of all possible next nodes
-        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
-        self.weights has all the weights between two nodes,
-        with the two nodes as a tuple as the key
-        e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
-        """
+
         self.edges = defaultdict(list)
         self.weights = {}
     
     def add_edge(self, from_node, to_node, weight):
-        # Note: assumes edges are bi-directional
         if(from_node in connections[to_node] and to_node in connections[from_node]):
             self.edges[from_node].append(to_node)
             self.weights[(from_node, to_node)] = weight
@@ -102,8 +43,6 @@ class Graph():
             self.edges[from_node].append(to_node)
             self.weights[(from_node, to_node)] = weight
             
-def replace_str_index(text,index=0,replacement=''):
-    return '%s%s%s'%(text[:index],replacement,text[index+1:])
  
 
 def dijsktra(graph, initial, end):
