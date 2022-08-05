887d86e2689ca302b18d9df0d18154761b9dcb67
Developful <preciseful1@gmail.com>
2022-08-05 23:18:59 +0300

    Added heat street

---
+++ distance heat street/MainFile.py
@@ -0,0 +1,12 @@
+import dijkstraFile
+Array = dijkstraFile.Array;
+Vector = dijkstraFile.Vector;
+
+# ^^^ Dont modify above ^^^
+
+
+#Node Positions
+nodePOS = Array(Vector(0, 0, 0))
+
+#Edges here
+connections = Array(Array(1, 2))
